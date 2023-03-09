8c8b477476da4ad997d30c667b724cd03c7e9cb6
Rodrigo Matias <souza89.teleco@gmail.com>
2023-03-09 12:43:59 +1100

    VIdeo Trailer  updates
    Video Pause and Current Time

--- VideoTrailerPopup/app.js
+++ VideoTrailerPopup/app.js
@@ -3,40 +3,35 @@ const button = document.getElementById("button")
 const video = document.getElementById("video")
 const content = document.getElementById("content")
 const xbutton = document.getElementById("xButton")
+const vid = document.getElementById("vid")
 
 
 
 
 
 button.addEventListener("click", (event) => { 
-     if(video.classList) 
-      video.classList.remove("hidden")
-     /*  video.classList.add("active"); */
-     else video.classList +=" active";  
-     if(xbutton.classList) 
-       xbutton.classList.remove("hidden")
-    /*  video.classList.add("active"); */
-    else video.classList +=" active"; 
-
+     if(video.classList)  video.classList.remove("hidden")
+       /*  else video.classList +=" active";  */ 
+     if(xbutton.classList) xbutton.classList.remove("hidden")
+      /*   else video.classList +=" active"; */ 
      if(content.classList) content.classList.add("hidden");
-     else video.classList +=" hidden"; 
+      /*   else video.classList +=" hidden";  */
       
-})
+});
 
 
 xbutton.addEventListener("click", (event) => {
 
-  if(video.classList) 
-  video.classList.add("hidden")
- /*  video.classList.add("active"); */
- else video.classList +=" hidden";  
- if(xbutton.classList) 
-   xbutton.classList.add("hidden")
-/*  video.classList.add("active"); */
-else video.classList +=" hidden"; 
+  if(video.classList) video.classList.add("hidden")
+ /*    else video.classList +=" hidden";   */
+  if(xbutton.classList)  xbutton.classList.add("hidden") 
+    /* else video.classList +=" hidden";  */
+  if(content.classList) content.classList.remove("hidden")
+    /* else video.classList +=" active"; */
+  vid.pause(); 
+  vid.currentime = 0;
+   
+     
+});
 
- if(content.classList) content.classList.remove("hidden");
- else video.classList +=" active";
-}
- 
-)
+
